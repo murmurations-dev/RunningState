7f79d3baed46243686a83d74268f242f2ac1e551
Etienne Vautherin <etienne@vautherin.com>
2024-02-25 15:27:53 +0100

    Initial Commit

---
+++ Explorer/ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExplorerApp.swift
+//  Explorer
+//
+//  Created by Etienne Vautherin on 25/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
